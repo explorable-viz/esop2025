729c9a81f8cd8168888ad6aa06522bf1d4757626
JosephBond <jecb1998@googlemail.com>
2023-01-10 11:03:24 +0000

    Added alga example spago project to devbranch

---
+++ examples/alga-examples/output/Node.ReadLine/foreign.js
@@ -0,0 +1,51 @@
+// module Node.ReadLine
+
+import { createInterface } from "readline";
+
+export function createInterfaceImpl(options) {
+  return () => createInterface({
+    input: options.input,
+    output: options.output,
+    completer: options.completer && (line => {
+      const res = options.completer(line)();
+      return [res.completions, res.matched];
+    }),
+    terminal: options.terminal,
+    historySize: options.historySize,
+  });
+}
+
+export function close(readline) {
+  return () => {
+    readline.close();
+  };
+}
+
+export function prompt(readline) {
+  return () => {
+    readline.prompt();
+  };
+}
+
+export function question(text) {
+  return callback => readline => () => {
+    readline.question(text, result => {
+      callback(result)();
+    });
+  };
+}
+
+export function setPrompt(prompt) {
+  return readline => () => {
+    readline.setPrompt(prompt);
+  };
+}
+
+export function setLineHandler(callback) {
+  return readline => () => {
+    readline.removeAllListeners("line");
+    readline.on("line", line => {
+      callback(line)();
+    });
+  };
+}
