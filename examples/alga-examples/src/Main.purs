98098e1e3855c58e6ab3663ba9e9ef046dd937e3
JosephBond <jecb1998@googlemail.com>
2023-01-18 12:02:52 +0000

    Reorganizing project, starting to add unit tests

--- examples/alga-examples/src/Main.purs
+++ examples/alga-examples/src/Main.purs
@@ -14,6 +14,7 @@ import Data.Eq ((==))
 import Data.Function ((#))
 import Data.Functor (class Functor)
 import Data.List.Types (List(..), (:))
+import Data.List (zipWIth)
 import Data.Ord ((>=))
 import Data.Tuple (Tuple(..))
 import Data.Unfoldable (replicateA)
@@ -22,24 +23,6 @@ import Effect.Console (log)
 import Effect.Random (randomInt)
 import Node.ReadLine (createConsoleInterface, noCompletion, prompt, setLineHandler, setPrompt, close)
 import Prelude (class Applicative, Unit, bind, discard)
-
--- Add edge within state
-addEdge :: forall a. a -> a -> State (Graph a) (Graph a)
-addEdge s t = state (\g -> let e = edge s t in Tuple e (overlay g e))
-
-addNode :: forall a. a -> State (Graph a) (Graph a)
-addNode n = state (\g -> let newV = Vertex n in Tuple newV (overlay g newV))
-
-compareLists :: List Int -> List Int -> List Boolean
-compareLists (x:xs) (y:ys) = 
-  Cons (x >= y) (compareLists xs ys)
-compareLists _ (Cons _ _) = Nil
-compareLists _ _ = Nil
-
-boolList :: Int -> Int -> List Int -> Effect (List Boolean)
-boolList numNodes maxNum edgeCounts = do
-  randoms :: List Int <- replicateA numNodes (randomInt 1 maxNum)
-  pure (compareLists randoms edgeCounts)
   
 main :: Effect Unit
 main = do
--- examples/alga-examples/test/Main.purs
+++ examples/alga-examples/test/Main.purs
@@ -2,10 +2,35 @@ module Test.Main where
 
 import Prelude
 
+import Algebra.Graph (Graph(..), edge, overlay)
+import Algebra.Graph.AdjacencyMap (vertices, clique)
+import Algebra.Graph.Internal (fromArray)
+import Control.Apply (class Apply)
+import Control.Bind (class Bind)
+import Control.Monad (pure)
+import Control.Monad.State (State)
+import Control.Monad.State.Trans (StateT(..))
+import Control.Monad.State.Class (state)
+import Data.Eq ((==))
+import Data.Function ((#))
+import Data.Functor (class Functor)
+import Data.List.Types (List(..), (:))
+import Data.List (zipWIth)
+import Data.Ord ((>=))
+import Data.Tuple (Tuple(..))
+import Data.Unfoldable (replicateA)
+import Graph.Utils (compareLists)
+
 import Effect (Effect)
-import Effect.Class.Console (log)
+import Effect.Console (log)
+-- Test Imports
+import Test.Unit (suite, test)
+import Test.Unit.Assert as Assert
+import Test.Unit.Main (runTest)
+
 
 main :: Effect Unit
-main = do
-  log "🍝"
-  log "You should add some tests."
+main = runTest do
+  suite "graph utils" do
+    test "compareLists ex" do
+    Assert.equal (compareLists (1:2:3:4:Nil) (2:2:4:5:Nil)) (True : True : False : False : Nil)
