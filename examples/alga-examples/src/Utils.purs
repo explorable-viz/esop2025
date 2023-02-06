c1a1a5ac6aabe644b2da4f8a574f13195c5807d2
JosephBond <jecb1998@googlemail.com>
2023-02-06 13:22:46 +0000

    moved broken version of code to DEPRECATED file for posterity, refactored Utils.purs

--- examples/alga-examples/src/Utils.purs
+++ examples/alga-examples/src/Utils.purs
@@ -1,14 +1,11 @@
 module Graph.Utils where
 
-import Algebra.Graph (Graph(..), connect, edge, edgeCount, foldg, overlay, transpose, vertex, vertexCount, vertices)
+import Algebra.Graph (Graph, connect, edgeCount, foldg, overlay, transpose, vertex, vertexCount, vertices)
 import Algebra.Graph.AdjacencyMap as AM
 import Algebra.Graph.Internal (fromArray)
 import Control.Monad (pure)
-import Control.Monad.State (State)
-import Control.Monad.State.Class (state)
-import Control.Monad.State.Trans (StateT(..))
+import Control.Monad.State
 import Data.Array (filter, fromFoldable, sortWith, take, zip, zipWith)
-import Data.Function (($))
 import Data.Functor (map)
 import Data.Map (Map, intersectionWith, values)
 import Data.Newtype (unwrap)
@@ -18,9 +15,8 @@ import Data.Traversable (traverse)
 import Data.Tuple (Tuple(..), fst, snd)
 import Data.Unfoldable (replicateA)
 import Effect (Effect)
-import Effect.Class (liftEffect)
 import Effect.Random (random, randomInt)
-import Prelude (bind, (<<<), (<$>), (+), (*))
+import Prelude (bind, (<<<), (<$>), (+), (*), discard)
 
 
 
@@ -39,15 +35,14 @@ newNeighbours :: Int -> Int -> Array Int -> Int -> Effect (Array Int)
 newNeighbours numNodes maxNum nodeDegrees m = do
   randoms <- replicateA numNodes (randomInt 1 maxNum)        -- imperative random numbers
   let
-      flags          = compareArrays randoms nodeDegrees     -- Array Boolean
-      selectionPairs = zip nodeDegrees flags                 -- Array (Tuple Int Boolean)
-      selected       = map fst (filter snd selectionPairs)   -- Array Int
-      shuffled       = shuffle selected                      -- Effect (Array Int) imperative because of randoms
+      flags          = compareArrays randoms nodeDegrees     :: Array Boolean
+      selectionPairs = zip nodeDegrees flags                 :: Array (Tuple Int Boolean)
+      selected       = map fst (filter snd selectionPairs)   :: Array Int
+      shuffled       = shuffle selected                      :: Effect (Array Int) -- imperative because of randoms
   take m <$> shuffled
 
--- Next 2 functions are versions of the code which should work to simulate the barabasi albert model
-baNewnode'' :: Int -> (Graph Int -> Effect (Tuple (Graph Int) (Graph Int))) -- State (Graph Int) (Graph Int)
-baNewnode'' m = (\prev ->
+deltaGraph :: Int -> Graph Int -> Effect (Graph Int) -- State (Graph Int) (Graph Int)
+deltaGraph m prev =
   do
     let
       normalizer      = 2 * (edgeCount prev)
@@ -55,15 +50,20 @@ baNewnode'' m = (\prev ->
       degrees         = fromFoldable (values (totDegrees prev))
     neighbours :: Array Int <- newNeighbours (vertexCount prev) normalizer degrees m
     let
-      diffGraph  = addVertex' newId neighbours
-      newGraph  = overlay diffGraph prev
-    pure (Tuple (diffGraph) (newGraph)))
+      diffGraph  = outStarG newId neighbours
+    pure diffGraph
 
 baNewNodeST :: Int -> StateT (Graph Int) Effect (Graph Int)
-baNewNodeST m = StateT (baNewnode'' m)
+baNewNodeST m = do
+  prev <- get
+  diffNew <- lift (deltaGraph m prev)
+  let
+    newGraph = overlay prev diffNew
+  put newGraph
+  pure diffNew
 
 
--- Utilities Which Make baNewNode'' and baNewNodeST work 
+-- Utilities Which Make deltaGraph and baNewNodeST work
 -- Needed to reexport these for constructing degree functions
 toAdjacencyMap :: forall a. Ord a => Graph a -> AM.AdjacencyMap a
 toAdjacencyMap = foldg AM.empty AM.vertex AM.overlay AM.connect
@@ -77,63 +77,6 @@ inDegrees g = outDegrees (transpose g)
 totDegrees :: forall a. Ord a => Graph a -> Map a Int
 totDegrees g = intersectionWith (+) (inDegrees g) (outDegrees g)
 
-
-addVertex' :: Int -> Array Int -> Graph Int
-addVertex' newId neighbours = connect (vertex newId) (vertices (fromArray neighbours))
-
-
--- Unused functions from prior versions of the code
--- Add edge within state
-addEdge :: forall a. a -> a -> State (Graph a) (Graph a)
-addEdge s t = state (\g -> let e = edge s t in Tuple e (overlay g e))
-
-addNode :: forall a. a -> State (Graph a) (Graph a)
-addNode n = state (\g -> let newV = Vertex n in Tuple newV (overlay g newV))
-
-addVertex :: Graph Int -> Int -> Array Int -> Tuple (Graph Int) (Graph Int)
-addVertex prevGraph newNodeId neighbours = Tuple diffGraph newGraph
-  where
-    diffGraph = connect (vertex newNodeId) (vertices (fromArray neighbours))
-    newGraph  = overlay prevGraph diffGraph
-
--- Initial attempt at wrapping addvertex in StateT, pointless
-addVertexST :: Graph Int -> Int -> Array Int -> StateT (Graph Int) Effect (Graph Int)
-addVertexST prev newId neighbours = state (\_ -> Tuple newEdges newGraph)
-  where
-    newEdges = (connect (vertex newId) (vertices (fromArray neighbours)))
-    newGraph = overlay prev newEdges
-
--- 2 versions which don't work but pass typechecker
-baNewnode :: Graph Int -> Int -> StateT (Graph Int) Effect (Graph Int)
-baNewnode prev m =
-  let
-    normalizer      = edgeCount prev
-    newId           = 1 + (vertexCount prev)
-    degrees         = fromFoldable (values (outDegrees prev))
-  in do
-    neighbours <- liftEffect $ newNeighbours (vertexCount prev) normalizer degrees m
-    addVertexST prev newId neighbours
-
-baNewnode' :: Int -> Graph Int -> StateT (Graph Int) Effect (Graph Int)
-baNewnode' m prev =
-  let
-    normalizer      = edgeCount prev
-    newId           = 1 + (vertexCount prev)
-    degrees         = fromFoldable (values (outDegrees prev))
-  in do
-    neighbours <- liftEffect $ newNeighbours (vertexCount prev) normalizer degrees m
-    let
-      diffGraph = addVertex' newId neighbours
-      newGraph  = overlay diffGraph
-    state (\s -> Tuple diffGraph (newGraph s))
-
--- typechecking the newNeighbours function, as I was getting errors in the monad stack
-neighboursTT :: Int -> Graph Int ->  Effect (Array Int)
-neighboursTT m prev = 
-  let
-    normalizer      = edgeCount prev
-    -- newId           = 1 + (vertexCount prev)
-    degrees         = fromFoldable (values (outDegrees prev))
-  in do
-    neighbours <- newNeighbours (vertexCount prev) normalizer degrees m 
-    pure neighbours
+-- deltaGraph construction
+outStarG :: Int -> Array Int -> Graph Int
+outStarG newId neighbours = connect (vertex newId) (vertices (fromArray neighbours))
