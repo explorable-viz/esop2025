0e541130a19f34af745523ef963a88b3fb721839
Joe Bond <jecb1998@googlemail.com>
2023-10-25 16:57:31 +0100

    Fixed up summary stats :)

---
+++ summary_stats.py
@@ -0,0 +1,29 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def decompose_benchmarks():
+    benchmarks = pd.read_csv('../fluid/Benchmarks/benchmarks.csv', skipinitialspace=True, delimiter=',', index_col='Test-Name')
+    slicing = benchmarks.filter(like='slicing', axis='index')
+    graphics = benchmarks.filter(like='graphics', axis='index')
+    desugar = benchmarks.filter(like='desugar', axis='index')
+    misc = benchmarks.drop(pd.concat([slicing, graphics, desugar]).index, inplace=False)
+    print("All Benchmarks: ")
+    print (benchmarks[['Trace-Eval', 'Graph-Eval']].to_string())
+    benchspeedup = speedups(benchmarks)[['Eval-Speedup', 'Bwd-Speedup', 'Fwd-Speedup']]
+    print(benchspeedup.to_string())
+    print(benchspeedup.mean())
+    # for test_set in [slicing, graphics, desugar, misc]:
+    #     spedup = speedups(test_set)[['Eval-Speedup', 'Bwd-Speedup', 'Fwd-Speedup']]
+    #     print(spedup)
+    #     print(spedup.mean())
+
+
+
+def speedups(df):
+    df['Eval-Speedup'] = df['Trace-Eval'] / df['Graph-Eval']
+    df['Bwd-Speedup'] = df['Trace-Bwd'] / df['Graph-Bwd']
+    df['Fwd-Speedup'] = df['Trace-Fwd'] / df['Graph-Fwd']
+    out = df.replace([np.inf], np.nan, inplace=False)
+    return out
+decompose_benchmarks()
