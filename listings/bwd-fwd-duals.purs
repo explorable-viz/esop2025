b30fe9839cbc6483731cf9555f98deea0ef69043
Joe Bond <jecb1998@googlemail.com>
2023-10-30 15:15:09 +0000

    Discussion of op vs negations

--- listings/bwd-fwd-duals.purs
+++ listings/bwd-fwd-duals.purs
@@ -1,5 +1,5 @@
-bwdSliceDual :: forall g. Graph g => Set Vertex -> g -> g
-bwdSliceDual αs0 g0 = fwdSlice αs0 (op g0)
+bwdDualAsFwdOp :: forall g. Graph g => Set Vertex -> g -> g
+bwdDualAsFwdOp $\alpha$s0 g0 = fwdSlice $\alpha$s0 (op g0)
 
 fwdDualAsBwdOp :: forall g. Graph g => Set Vertex -> g -> g
-fwdDualAsBwdOp αs0 g0 = bwdSlice αs0 (op g0)
+fwdDualAsBwdOp $\alpha$s0 g0 = bwdSlice $\alpha$s0 (op g0)
