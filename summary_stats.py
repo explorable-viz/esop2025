61b2a087a22f9a4a22c4e9de22c7b130133c498d
JosephBond <jecb1998@googlemail.com>
2024-02-19 08:37:54 +0000

    Def of matrix cases

--- summary_stats.py
+++ summary_stats.py
@@ -2,6 +2,13 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+matrix_cases = [
+    "slicing/dtw/compute-dtw",
+    "slicing/convolution/edgeDetect",
+    "slicing/convolution/emboss",
+    "slicing/convolution/gaussian"
+]
+
 def decompose_benchmarks():
     benchmarks = pd.read_csv('../fluid/Benchmarks/benchmarks.csv', skipinitialspace=True, delimiter=',', index_col='Test-Name')
     slicing = benchmarks.filter(like='slicing', axis='index')
@@ -19,23 +26,23 @@ def decompose_benchmarks():
 def eval_speedup(df):
     df['Eval-Speedup'] = df['T-Eval'] / df['G-Eval']
     out = df[['T-Eval', 'G-Eval', 'Eval-Speedup']]
-    tex_file = open('fig/performance/eval-speedup.tex', 'w')
-    tex_file.write(out.to_latex(float_format="%.2f", caption = "Slicing test-case evaluation times and average speedup", label='table:eval-speedups', longtable=True))
-    tex_file.close()
+    #tex_file = open('fig/performance/eval-speedup.tex', 'w')
+    #tex_file.write(out.to_latex(float_format="%.2f", caption = "Slicing test-case evaluation times and average speedup", label='table:eval-speedups', longtable=True))
+    #tex_file.close()
     return out
 
 def equiv_implementations(df):
     out = df[['G-Fwd', 'Naive-Fwd','G-BwdDlCmp', 'G-BwdDlFwdOp', 'G-FwdDlCmp', 'G-FwdDlBwdOp']]
     tex_file = open('fig/performance/equivalent-impls.tex', 'w')
-    tex_file.write(out.to_latex(float_format="%.2f", caption = "Equivalent implementations of the De Morgan dual", label='table:equivalent-impls', longtable=True))
-    tex_file.close()
+    #tex_file.write(out.to_latex(float_format="%.2f", caption = "Equivalent implementations of the De Morgan dual", label='table:equivalent-impls', longtable=True))
+    #tex_file.close()
 
 def slicing_times(df):
     df['Bwd-Speedup'] = df['T-Bwd'] / df['G-Bwd']
     df['Fwd-Speedup'] = df['T-Fwd'] / df['G-Fwd']
     out = df[['T-Fwd','G-Fwd','Fwd-Speedup','T-Bwd','G-Bwd','Bwd-Speedup']]
-    tex_file = open('fig/performance/slicing-speedup.tex', 'w')
-    tex_file.write(out.to_latex(float_format="%.2f", caption="Slicing times and average speedups", label='table:slicing-speedups', longtable=True))
+    #tex_file = open('fig/performance/slicing-speedup.tex', 'w')
+    #tex_file.write(out.to_latex(float_format="%.2f", caption="Slicing times and average speedups", label='table:slicing-speedups', longtable=True))
     return out
 
 def speedups(df):
