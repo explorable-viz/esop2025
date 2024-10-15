99b8b09a311cd9ad9b771f9a55c5e79e89c9df31
Roly Perera <roly.perera@dynamicaspects.org>
2024-10-15 14:05:58 +0100

    Progress on script.

--- arXiv.sh
+++ arXiv.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 set -xe
 
+NAME=arXiv.zip
 # . build.sh
-zip -r arXiv.zip .
 
+pushd ..
+
+zip -r - $NAME graphical-slicing > $NAME
+
+# zip -d $NAME *.zip
+zip -d $NAME *.pdf
+zip -d $NAME *.DS_Store
+zip -d $NAME *.git
+zip -d $NAME *.gitignore
+zip -d $NAME graphical-slicing/Talk/\*
+
+ls -al $NAME
+unzip -l $NAME
+
+popd
