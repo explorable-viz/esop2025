1b185d2302bbc9fc93e45230dbe52982b8c54bee
Roly Perera <roly.perera@dynamicaspects.org>
2024-10-15 15:02:31 +0100

    Need top-level target for arXiv, couldn't get it to build properly otherwise..

--- arXiv.sh
+++ arXiv.sh
@@ -2,11 +2,11 @@
 set -xe
 
 NAME=arXiv.zip
-TARGET=main-full
+TARGET=arXiv
 
-# pdflatex $TARGET
-# biber $TARGET
-# rm -f $TARGET.aux $TARGET.dvi $TARGET.log $TARGET.blg $TARGET.out $TARGET.pag $TARGET.cb $TARGET.cb2 $TARGET.toc
+pdflatex $TARGET
+biber $TARGET
+rm -f $TARGET.aux $TARGET.dvi $TARGET.log $TARGET.blg $TARGET.out $TARGET.pag $TARGET.cb $TARGET.cb2 $TARGET.toc
 
 pushd ..
 
@@ -18,6 +18,11 @@ zip -d $NAME \*.DS_Store
 zip -d $NAME \*.gitignore
 zip -d $NAME \*.gitmodules
 zip -d $NAME \*.purs-repl
+zip -d $NAME graphical-slicing/appendix.tex
+zip -d $NAME graphical-slicing/main-full.tex
+zip -d $NAME graphical-slicing/main.tex
+zip -d $NAME graphical-slicing/paper.tex
+zip -d $NAME graphical-slicing/inductive-graphs.tex
 zip -d $NAME graphical-slicing/author-response/\*
 zip -d $NAME graphical-slicing/Benchmarks/\*
 zip -d $NAME graphical-slicing/tex-common/bib.bib
@@ -26,6 +31,7 @@ zip -d $NAME graphical-slicing/venv/\*
 zip -d $NAME graphical-slicing/mechanisation/\*
 zip -d $NAME graphical-slicing/obsolete/\*
 zip -d $NAME graphical-slicing/Talk/\*
+zip -d $NAME graphical-slicing/.vscode/\*
 
 ls -al $NAME
 unzip -l $NAME
