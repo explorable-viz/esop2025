f2def502adadaddbc0a823c6b0eee5b8b38cf41e
Roly Perera <roly.perera@dynamicaspects.org>
2024-10-15 14:18:46 +0100

    More progress on script.

--- arXiv.sh
+++ arXiv.sh
@@ -2,17 +2,24 @@
 set -xe
 
 NAME=arXiv.zip
-# . build.sh
+TARGET=main-full
+
+# pdflatex $TARGET
+# biber $TARGET
+# rm -f $TARGET.aux $TARGET.dvi $TARGET.log $TARGET.blg $TARGET.out $TARGET.pag $TARGET.cb $TARGET.cb2 $TARGET.toc
 
 pushd ..
 
 zip -r - $NAME graphical-slicing > $NAME
 
-# zip -d $NAME *.zip
-zip -d $NAME *.pdf
-zip -d $NAME *.DS_Store
-zip -d $NAME *.git
-zip -d $NAME *.gitignore
+zip -d $NAME \*.zip
+zip -d $NAME \*.pdf
+zip -d $NAME \*.DS_Store
+zip -d $NAME \*.gitignore
+zip -d $NAME graphical-slicing/.git/\*
+zip -d $NAME graphical-slicing/venv/\*
+zip -d $NAME graphical-slicing/mechanisation/\*
+zip -d $NAME graphical-slicing/obsolete/\*
 zip -d $NAME graphical-slicing/Talk/\*
 
 ls -al $NAME
