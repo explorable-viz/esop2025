611c90239e62f1fb22ac2cd770b63406ff61dc26
Roly Perera <roly.perera@dynamicaspects.org>
2025-01-30 14:47:00 +0000

    zip script: inline outer 'esop2025' directory.

--- arXiv.sh
+++ arXiv.sh
@@ -4,37 +4,39 @@ set -xe
 NAME=arXiv.zip
 TARGET=arXiv
 FOLDER=esop2025
+ARCHIVE="../$NAME"
 
 pdflatex $TARGET
 biber $TARGET
 rm -f $TARGET.aux $TARGET.dvi $TARGET.log $TARGET.blg $TARGET.out $TARGET.pag $TARGET.cb $TARGET.cb2 $TARGET.toc $TARGET.bcf $TARGET.run.xml
 
-pushd ..
+# pushd ..
 
-zip -r - $NAME $FOLDER > $NAME
+# zip -r - $NAME $FOLDER > $NAME
+zip -r - . > $ARCHIVE
 
-zip -d $NAME \*.zip
-zip -d $NAME \*.pdf
-zip -d $NAME \*.DS_Store
-zip -d $NAME \*.gitignore
-zip -d $NAME \*.gitmodules
-zip -d $NAME \*.purs-repl
-zip -d $NAME $FOLDER/appendix.tex
-zip -d $NAME $FOLDER/main-full.tex
-# zip -d $NAME $FOLDER/main.tex
-# zip -d $NAME $FOLDER/paper.tex
-zip -d $NAME $FOLDER/inductive-graphs.tex
-zip -d $NAME $FOLDER/author-response/\*
-zip -d $NAME $FOLDER/Benchmarks/\*
-# zip -d $NAME $FOLDER/tex-common/bib.bib
-zip -d $NAME $FOLDER/.git/\*
-zip -d $NAME $FOLDER/venv/\*
-zip -d $NAME $FOLDER/mechanisation/\*
-zip -d $NAME $FOLDER/obsolete/\*
-zip -d $NAME $FOLDER/Talk/\*
-zip -d $NAME $FOLDER/.vscode/\*
+# zip -d $ARCHIVE \*.zip
+zip -d $ARCHIVE \*.pdf
+zip -d $ARCHIVE \*.DS_Store
+zip -d $ARCHIVE \*.gitignore
+zip -d $ARCHIVE \*.gitmodules
+zip -d $ARCHIVE \*.purs-repl
+zip -d $ARCHIVE appendix.tex
+zip -d $ARCHIVE main-full.tex
+# zip -d $ARCHIVE $main.tex
+# zip -d $ARCHIVE $paper.tex
+zip -d $ARCHIVE inductive-graphs.tex
+zip -d $ARCHIVE author-response/\*
+zip -d $ARCHIVE Benchmarks/\*
+# zip -d $ARCHIVE $tex-common/bib.bib
+zip -d $ARCHIVE .git/\*
+zip -d $ARCHIVE venv/\*
+zip -d $ARCHIVE mechanisation/\*
+zip -d $ARCHIVE obsolete/\*
+zip -d $ARCHIVE Talk/\*
+zip -d $ARCHIVE .vscode/\*
 
-ls -al $NAME
-unzip -l $NAME
+ls -al $ARCHIVE
+unzip -l $ARCHIVE
 
-popd
+# popd
